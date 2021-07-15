d4c6e0f7a9a74da607f4af5e7713ee884766f2d1
Prabhakar <prabhakar@knowhowhere.com>
2021-07-16 09:09:00 +1000

    adding_all_post

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -9,7 +9,7 @@ def starting_page(request):
 	return render(request, 'blog/index.html')
 
 def posts(request):
-	return HttpResponse('hello from the blog page')
+	return render(request, 'blog/all-posts.html')
 
 def post_details(request):
 	pass
