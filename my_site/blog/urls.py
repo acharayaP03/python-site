06224ecaed306e17e0d3cd799f50233983f1c469
acharayaP03 <acharyaP03@gmail.com>
2021-08-01 18:25:17 +1000

    migrating functional views to class based views

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path("", views.starting_page, name="starting-page"),
-	path("posts", views.posts, name="posts-page"),
-	path("posts/<slug:slug>", views.post_details, name="post-detail-page")
+	# path("", views.starting_page, name="starting-page"),
+	path("", views.StartingPage.as_view(), name="starting-page"),
+	# path("posts", views.posts, name="posts-page"),
+	path("posts", views.AllPostview.as_view(), name="posts-page"),
+	# path("posts/<slug:slug>", views.post_details, name="post-detail-page")
+	path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page")
 ]
