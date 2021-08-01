de6e704d8df6f75a7a034980c5e38c64e221ff4f
acharayaP03 <acharyaP03@gmail.com>
2021-08-01 18:48:38 +1000

    Create Comment model

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -28,3 +28,14 @@ class Post(models.Model):
     # setting many to many relation with tag model
     tags = models.ManyToManyField(Tag)
 
+
+"""
+Comment Model will have one to many relation with Post, hence we will access it 
+with Foreign key 
+"""
+
+class Comment(models.Model):
+    user_name = models.CharField(max_length=120)
+    user_email= models.EmailField()
+    text= models.TextField(max_length=400)
+    post=models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
